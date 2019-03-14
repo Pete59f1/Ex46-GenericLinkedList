0beb2bfaf3dda43c93329f11806b94776ca2dd87
Peter Starup <pete59f1@edu.eal.dk>
2019-03-14 10:40:33 +0100

    Lavet en opgave angående extension methods. Ex60 ExtensionMethods

---
+++ ADT/ADTExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    public static class ADTExtension
+    {
+        public static void ForEach<T>(this MyLinkedList<T> list, Action<T> action) where T : IComparable<T>
+        {
+            foreach (T item in list)
+            {
+                action(item);
+            }
+        }
+    }
+}
